d8fbaebfc0e20829dce78484e0ca98d778f5b0e2
Gulsong <64633342+gulsong@users.noreply.github.com>
2021-05-17 19:40:05 +0800

    Add files via upload

---
+++ pro/js/rem.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    var html = document.querySelector("html");
+    html.style.fontSize = html.offsetWidth / 750 * 100 + "px";
+    addEventListener("resize", function() {
+        html.style.fontSize = html.offsetWidth / 750 * 100 + "px";
+    }, false);
+}
